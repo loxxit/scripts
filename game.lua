bf2626e745f8e8ef465bc9b4db63b055327d0ba9
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 23:34:22 +0300

    Update game.lua

--- game.lua
+++ game.lua
@@ -1,3 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/getter.lua"))()
+
+
 getgenv().SecureMode = true
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({

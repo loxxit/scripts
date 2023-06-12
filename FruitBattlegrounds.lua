f1c8d7315054fbabf6e9d8de67bec7b2949a610f
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 21:36:36 +0300

    Delete FruitBattlegrounds.lua

--- FruitBattlegrounds.lua
+++
@@ -1,2 +0,0 @@
-game:GetService("Players").LocalPlayer:Kick("Discountinued More INFO: gg/F2GnrCCADJ")
---Discountinued if you are looking from github do not execute this you should understand what is going on the above if you have english ☠💀

7ba89937056732fef183269ac96ec28083b13447
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 22:10:25 +0300

    Update spin.lua

--- spin.lua
+++ spin.lua
@@ -4,6 +4,7 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 local playerData = replicatedStorage:WaitForChild("Player_Data")
 local localData = playerData:WaitForChild(localPlayer.Name)
 local clan = localData:WaitForChild("Clan")
+local spins = playerData:WaitForChild(localPlayer.Name).Spins
 local remotes = replicatedStorage:WaitForChild("Remotes")
 local sendServer = remotes:WaitForChild("To_Server")
 local handleSpin = sendServer:WaitForChild("Handle_Initiate_S_")
@@ -20,26 +21,30 @@ while true do
       end
    end
 
-   if shouldSpin then
-      handleSpin:InvokeServer("check_can_spin")
-   else
-      local Rayfield = require(game:GetService("ReplicatedStorage"):WaitForChild("Rayfield"))
-      local notification = Rayfield:Notify({
-         Title = "You Got " .. clan.Value,
-         Content = "You Got " .. clan.Value .. " and stopped the spinning.",
-         Duration = 6.5,
-         Image = 4483362458,
-         Actions = { -- Notification Buttons
-            Ignore = {
-               Name = "Okay!",
-               Callback = function()
-                  -- Add any desired callback functionality here
-               end
+   if spins.Value > 0 then
+      if shouldSpin then
+         handleSpin:InvokeServer("check_can_spin")
+      else
+         local notification = Rayfield:Notify({
+            Title = "You Got " .. clan.Value,
+            Content = "You Got " .. clan.Value .. " and stopped the spinning.",
+            Duration = 6.5,
+            Image = 4483362458,
+            Actions = { -- Notification Buttons
+               Ignore = {
+                  Name = "Okay!",
+                  Callback = function()
+                     -- Add any desired callback functionality here
+                  end
+               }
             }
-         }
-      })
-      notification:Destroy()
-      break -- Exit the loop when spinning stops
+         })
+         notification:Destroy()
+         break -- Exit the loop when spinning stops
+      end
+   else
+      print("No spins left")
+      break -- Exit the loop when no spins left
    end
 
    wait(0.001)

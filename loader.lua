51180ecf8886b15f3c283a08489f106b77671aff
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 23:53:03 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,9 +3,9 @@ local ouwhana = 11468159863
 local map1 = 6152116144
 local ouwg = 11468075017
 
-if game.GameId == ouwg or ouwhana or map1 then
+if game.PlaceId == ouwg or ouwhana or map1 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/game.lua"))()
- elseif game.GameId == menu then
+ elseif game.PlaceId == menu then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/menu.lua"))()
  else 
    game.Players.LocalPlayer:Kick("Not Supported")

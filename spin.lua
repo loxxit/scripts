e06bbecf26cd1f33af754d5e418731e5c7ed9bc9
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 22:28:52 +0300

    Update spin.lua

--- spin.lua
+++ spin.lua
@@ -1,16 +1,3 @@
-local players = game:GetService("Players")
-local localPlayer = players.LocalPlayer
-local replicatedStorage = game:GetService("ReplicatedStorage")
-local playerData = replicatedStorage:WaitForChild("Player_Data")
-local localData = playerData:WaitForChild(localPlayer.Name)
-local clan = localData:WaitForChild("Clan")
-local spins = playerData:WaitForChild(localPlayer.Name).Spins
-local remotes = replicatedStorage:WaitForChild("Remotes")
-local sendServer = remotes:WaitForChild("To_Server")
-local handleSpin = sendServer:WaitForChild("Handle_Initiate_S_")
-
-local selectedOptions = getgenv().selectedOptions or {}
-
 while true do
    local shouldSpin = true
 
@@ -25,37 +12,39 @@ while true do
       if shouldSpin == true then
          handleSpin:InvokeServer("check_can_spin")
       else
+         -- Display a notification indicating the spinning has stopped
          Rayfield:Notify({
-            Title = "You Got: "..clan.Value,
-            Content = "You Got: "..clan.Value..", Stopped Spinning.",
+            Title = "Spinning Stopped",
+            Content = "You obtained the selected clan: " .. clan.Value,
             Duration = 6.5,
             Image = 4483362458,
-            Actions = { -- Notification Buttons
+            Actions = {
                Ignore = {
-               Name = "Okay!",
-               Callback = function()
-                  print("The user tapped Okay!")
-      end
-   },
-},
-})
+                  Name = "Okay!",
+                  Callback = function()
+                     print("The user tapped Okay!")
+                  end
+               }
+            }
+         })
          break -- Exit the loop when spinning stops
       end
    else
+      -- Display a notification indicating there are no spins left
       Rayfield:Notify({
-            Title = "No Spns left. You Got: "..clan.Value,
-            Content = "Stopped Spinning.",
-            Duration = 6.5,
-            Image = 4483362458,
-            Actions = { -- Notification Buttons
-               Ignore = {
+         Title = "No Spins Left",
+         Content = "Spinning Stopped",
+         Duration = 6.5,
+         Image = 4483362458,
+         Actions = {
+            Ignore = {
                Name = "Okay!",
                Callback = function()
                   print("The user tapped Okay!")
-      end
-   },
-},
-})
+               end
+            }
+         }
+      })
       break -- Exit the loop when no spins left
    end
 

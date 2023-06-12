26a05ef08fa088c0413d8d83d5701b437d01d8ca
loxx <112658304+loxxit@users.noreply.github.com>
2023-06-12 21:54:07 +0300

    Update spin.lua

--- spin.lua
+++ spin.lua
@@ -1,33 +1,32 @@
-local players = game:GetService("Players");
-local runService = game:GetService("RunService");
-local replicatedStorage = game:GetService("ReplicatedStorage");
+local players = game:GetService("Players")
+local localPlayer = players.LocalPlayer
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local playerData = replicatedStorage:WaitForChild("Player_Data")
+local localData = playerData:WaitForChild(localPlayer.Name)
+local clan = localData:WaitForChild("Clan")
+local remotes = replicatedStorage:WaitForChild("Remotes")
+local sendServer = remotes:WaitForChild("To_Server")
+local handleSpin = sendServer:WaitForChild("Handle_Initiate_S_")
 
-local localPlayer = players.LocalPlayer;
-local playerData = replicatedStorage:WaitForChild("Player_Data");
-local localData = playerData:WaitForChild(localPlayer.Name);
-local clan = localData:WaitForChild("Clan");
-
-local remotes = replicatedStorage:WaitForChild("Remotes");
-local sendServer = remotes:WaitForChild("To_Server");
-local handleSpin = sendServer:WaitForChild("Handle_Initiate_S_");
-
-while task.wait(0.001) do
-   if not table.find(selectedOption, clan.Value) then
-           handleSpin:InvokeServer("check_can_spin");
+while true do
+   if not table.find(getgenv().selectedOption, clan.Value) then
+      handleSpin:InvokeServer("check_can_spin")
    else
       Rayfield:Notify({
-         Title = "You Got "..clan.Value,
-         Content = "You Got "..clan.Value.." and stopped the spinning.",
+         Title = "You Got " .. clan.Value,
+         Content = "You Got " .. clan.Value .. " and stopped the spinning.",
          Duration = 6.5,
          Image = 4483362458,
          Actions = { -- Notification Buttons
             Ignore = {
                Name = "Okay!",
                Callback = function()
-                 
-      end
-   },
-},
-})
+                  -- Add any desired callback functionality here
+               end
+            }
+         }
+      })
+      break -- Exit the loop when spinning stops
    end
+   task.wait(0.001)
 end

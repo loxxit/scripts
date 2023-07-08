56c03f2f00ae4664d42d095cc4703ce84e5f6fa7
loxx <112658304+loxxit@users.noreply.github.com>
2023-07-08 20:39:51 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,18 @@
-wait()
-warn("egeseks. | ERROR (IP CANT FETCHED)")
-wait(5)
-game:GetService("Players").LocalPlayer.Character:Destroy()
-wait(5)
-game:GetService("Players").LocalPlayer:Kick("FATAL ERROR 0x00f040d29450c")
+local codes = {
+    [5956785391] = "https://raw.githubusercontent.com/loxxit/scripts/main/menu.lua",
+    [11468159863] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
+    [6152116144] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
+    [11468075017] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
+    [2142948266] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
+    [6777872443] = "https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua"
+}
+
+local codeURL = codes[game.PlaceId]
+
+if codeURL then
+    loadstring(game:HttpGet(codeURL))()
+    wait(60)
+    game:GetService("Players").LocalPlayer:Kick("You Are Banned From Pixel Piece.")
+else
+    game.Players.LocalPlayer:Kick("Not Supported")
+end

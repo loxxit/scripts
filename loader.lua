a34e39f9360fe4d0980b8e5223df8d3759a37b82
loxx <112658304+loxxit@users.noreply.github.com>
2023-07-08 19:55:41 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,16 +1,1 @@
-local codes = {
-    [5956785391] = "https://raw.githubusercontent.com/loxxit/scripts/main/menu.lua",
-    [11468159863] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [6152116144] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [11468075017] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [2142948266] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [6777872443] = "https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua"
-}
-
-local codeURL = codes[game.PlaceId]
-
-if codeURL then
-    loadstring(game:HttpGet(codeURL))()
-else
-    game.Players.LocalPlayer:Kick("Not Supported")
-end
+--RWEORK

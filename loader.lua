eb98754eac48734c5a267848e95c9c0aa7c0c7c8
loxx <112658304+loxxit@users.noreply.github.com>
2023-08-08 01:34:11 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,14 +4,19 @@ local codes = {
     [6152116144] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
     [11468075017] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
     [2142948266] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [6777872443] = "https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua"
+    [6777872443] = "https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua",
     [6284583030] = "https://raw.githubusercontent.com/loxxit/scripts/main/mainyeetapet.lua"
 }
 
 local codeURL = codes[game.PlaceId]
 
 if codeURL then
-    loadstring(game:HttpGet(codeURL))()
+    local success, scriptContent = pcall(game.HttpGet, game, game, codeURL)
+    if success then
+        loadstring(scriptContent)()
+    else
+        game.Players.LocalPlayer:Kick("Error fetching script content")
+    end
 else
     game.Players.LocalPlayer:Kick("Not Supported")
 end

fd9c119da75855b31f47a4772adf4ef103ded989
loxx <112658304+loxxit@users.noreply.github.com>
2023-08-08 22:12:37 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,22 +1,9 @@
-local codes = {
-    [5956785391] = "https://raw.githubusercontent.com/loxxit/scripts/main/menu.lua",
-    [11468159863] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [6152116144] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [11468075017] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [2142948266] = "https://raw.githubusercontent.com/loxxit/scripts/main/game.lua",
-    [6777872443] = "https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua",
-    [6284583030] = "https://raw.githubusercontent.com/loxxit/scripts/main/mainyeetapet.lua"
-}
-
-local codeURL = codes[game.PlaceId]
-
-if codeURL then
-    local success, scriptContent = pcall(game.HttpGet, game, game, codeURL)
-    if success then
-        loadstring(scriptContent)()
-    else
-        game.Players.LocalPlayer:Kick("Error fetching script content")
-    end
-else
-    game.Players.LocalPlayer:Kick("Not Supported")
+if game.PlaceId == 11468159863 or 6152116144 or 11468075017 or 2142948266 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/game.lua"))()
+elseif game.PlaceId == 6284583030 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/mainyeetapet.lua"))()
+elseif game.PlaceId == 6777872443 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua"))()
+elseif game.PlaceId == 5956785391 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/menu.lua"))()
 end

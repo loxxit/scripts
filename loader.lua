8855c50f95dd9d28534605955812b002cc262260
loxx <112658304+loxxit@users.noreply.github.com>
2023-08-27 20:35:15 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,5 @@
 if game.PlaceId == 11468159863 or game.PlaceId == 6152116144 or  game.PlaceId == 11468075017 or  game.PlaceId == 2142948266 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/game.lua"))()
-elseif game.PlaceId == 6284583030 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/mainyeetapet.lua"))()
 elseif game.PlaceId == 6777872443 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/loxxit/scripts/main/pixelpiece.lua"))()
 elseif game.PlaceId == 5956785391 then
